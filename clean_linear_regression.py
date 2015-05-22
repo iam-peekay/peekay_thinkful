b16f2d9bdf948e5e7312b96277cde50c11053d24
Preethi Kasireddy <iam.preethi.k@gmail.com>
2015-05-22 16:07:40 -0700

    linear regression homework

---
+++ clean_linear_regression.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+
+cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
+loansData['Interest.Rate'] = cleanInterestRate
+loansData['Interest.Rate'][0:5]
+
+cleanLoanLength = loansData['Loan.Length'].map(lambda x: int(x.rstrip('months')))
+
+loansData['Loan.Length'] = cleanLoanLength
+ 
+cleanFICORange = loansData['FICO.Range'].map(lambda x: int(x.split('-')[0]))
+
+loansData['FICO.Range'] = cleanFICORange
+
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Range']
+
+y = np.matrix(intrate).transpose() #this is because python does multiplication in rows and not columns
+
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+
+x = np.column_stack([x1,x2])
+
+X = sm.add_constant(x)
+model = sm.OLS(y, X)
+f = model.fit()
+
+print f.summary()
+
+
